cce1a09c9263df132aa3af35fa548e4a3b8b9508
evhenii-volynets <hw.volynets@gmail.com>
2022-08-02 11:09:20 +0300

    base

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import './App.css';
-import Header from "./components/Header";
-
-const users = [
-    {
-        name: 'Vasya',
-        age: 10,
-        gender: true
-    },
-    {
-        name: 'Petys',
-        age: 20,
-        gender: true
-    },
-    {
-        name: 'Anya',
-        age: 100,
-        gender: false
-    }
-]
 
 function App() {
-    const changeColor = (e, color) => {
-        console.log(e.target)
-        e.target.style.color = color
-    }
-  return (
-    <div className="App">
-        <Header
-            click={changeColor}
-            title="Vasya Header"
-            component={() => <p>test component</p>}
-        >
-            <p>test title</p>
-        </Header>
-        <ul>
-            {users.map((el, key) =>
-                <li onClick={(e) => changeColor(e, 'green')} key={key} className={el.gender ? 'name-boy' : "name-girl"}>{el.name} - {el.age}</li>
-            )}
-        </ul>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
-
 export default App;
