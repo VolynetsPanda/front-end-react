20d8484e44d940f1a29f293d605e7b7de1161187
evhenii-volynets <hw.volynets@gmail.com>
2022-08-26 11:26:40 +0300

    Context

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
+import App from "./app";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ContextUser, ContextTask } from "store/context";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Init() {
+    const [user, setUser] = useState({})
+    const [task, setTask] = useState([])
+    return (
+        <React.StrictMode>
+            <ContextUser.Provider value={{user, setUser}}>
+                <ContextTask.Provider value={{task, setTask}}>
+                    <App/>
+                </ContextTask.Provider>
+            </ContextUser.Provider>
+        </React.StrictMode>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Init />, document.getElementById('root'));
+
+const devMode = process.env.NODE_ENV === "development"
+if(devMode && module && module.hot){
+    module.hot.accept()
+}
